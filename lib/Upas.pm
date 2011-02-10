aa2d6b73f0cc42daeff6a7e2a23652fdb6920402
YellowTurtle / satoshi azuma <ytnobody@gmail.com>
2011-02-10 16:22:39 +0900

    initial commit

---
+++ lib/Upas.pm
@@ -0,0 +1,32 @@
+package Upas;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Upas -
+
+=head1 SYNOPSIS
+
+  use Upas;
+
+=head1 DESCRIPTION
+
+Upas is
+
+=head1 AUTHOR
+
+satoshi azuma E<lt>ytnobody@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
